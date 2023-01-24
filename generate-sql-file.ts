148ec5f353dd24903180758f8af329e7de57606a
Unknown <gabriel.henrique@automatos.com>
2023-01-24 09:48:47 -0300

    first commit

---
+++ generate-sql-file.ts
@@ -0,0 +1,64 @@
+import fs from "fs";
+import path from "path";
+
+const jsonPath = path.resolve(__dirname, "json-generated.json");
+
+const RELEASE_ID = 0;
+const BUILD = "";
+
+type TranslateObject = {
+
+    "en-US": string
+    "pt-BR": string
+    "es": string
+}
+
+type Row = {
+
+    version: string
+    type: string
+    title: TranslateObject
+    description: TranslateObject
+    date: string
+}
+
+(async () => {
+
+    const buffer = fs.readFileSync(jsonPath);
+    const parsed = JSON.parse(buffer.toString()) as Row[];
+
+    let query = "";
+
+    for (const row of parsed) {
+
+        query += `
+
+
+INSERT INTO 
+    ciq.release (title, description, type, date, releaseId, build) 
+VALUES 
+    ('${JSON.stringify(row.title)}', '${JSON.stringify(row.description)}', '${row.type}', '${row.date}', ${RELEASE_ID}, '${BUILD}');
+
+
+        `
+    }
+
+    if (fs.existsSync("sql-generated.sql")) fs.unlinkSync("sql-generated.sql");
+
+    fs.writeFileSync("sql-generated.sql", query);
+
+})();
+
+// "version": "CIQ V1.1.1",
+// "type": "N",
+// "title": {
+//     "en-US": "Employees",
+//     "pt-BR": "Funcionários",
+//     "es": "Empleados"
+// },
+// "description": {
+//     "en-US": "A section for adding, editing and delete employees was added to Administrative Settings.",
+//     "pt-BR": "Uma seção para adicionar, editar e excluir funcionários foi adicionada às Configurações Administrativas.",
+//     "es": "Se agregó una sección para agregar, editar y eliminar empleados en Configuración administrativa."
+// },
+// "date": "2023-19-01"
---
+++ index.ts
@@ -0,0 +1,89 @@
+import fs from "fs";
+import path from "path";
+
+const csvPath = path.resolve(__dirname, "csv-file.csv");
+const jsonPath = path.resolve(__dirname, "json-generated.json");
+
+const JSON_REPLACER = null;
+const JSON_IDENTITY_SPACES = 4;
+
+const JSONrows = [];
+
+const typeMap = {
+
+    "New": "N",
+    "Improvement": "I",
+    "Fixes": "F",
+}
+
+function transformTypeFieldToDatabaseChar(csvType: string) {
+
+    if (csvType in typeMap) return typeMap[csvType as keyof typeof typeMap];
+
+    return "F";
+}
+
+function formatDate(stringifyDate: string) {
+
+    const digits = stringifyDate.split("/");
+
+    const yyyy = digits[2];
+
+    const mm = parseInt(digits[1]) <= 9 
+        ? "0" + digits[1]
+        : digits[1]
+
+    const dd = parseInt(digits[0]) <= 9 
+        ? "0" + digits[0]
+        : digits[0]
+
+    return `${yyyy}-${mm}-${dd}`;
+}
+
+function createTranslateObject(text: string) {
+
+    return {
+
+        "en-US": text,
+        "pt-BR": text,
+        "es": text
+    }
+}
+
+(async () => {
+
+
+    const buffer = fs.readFileSync(csvPath);
+    const stringify = buffer.toString();
+
+    const lines = stringify.split("\n");
+
+    for (const line of lines) {
+
+        if (line === "\r" || line === "\n" || line === "") continue;
+
+        const withoutCarryingReturns = line 
+            .replace("\n", "")
+            .replace("\r", "");
+
+        const [version, type, title, description, date] = withoutCarryingReturns.split("|");
+
+        console.log({
+            version, type, title, description, date
+        })
+
+        JSONrows.push({
+
+            version, 
+            type: transformTypeFieldToDatabaseChar(type),
+            title: createTranslateObject(title),
+            description: createTranslateObject(description),
+            date: formatDate(date)
+        });
+    }
+
+    if (fs.existsSync(jsonPath)) fs.rmSync(jsonPath);
+
+    fs.writeFileSync(jsonPath, JSON.stringify(JSONrows, JSON_REPLACER, JSON_IDENTITY_SPACES));
+
+})();
